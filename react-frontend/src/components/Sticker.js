232fd376574a3153d2332e5da45dc9794ce8f2c2
Vinit Shahdeo <vinitshahdeo@gmail.com>
2020-02-20 00:52:07 +0530

    squash commit for inline external to bootstrap
    improved the footer code-snippet
    
    updated the code-snippet at footer, missing braces
    
    fixed grammatical typo
    
    Create weekly-digest.yml
    
    Create auto_assign.yml
    
    Format 904ade7
    
    Added FAQs and resources
    
    fixed markdown
    
    beautified README.md
    
    Initial React boilerplate
    
    Conversion Into React
    
    Fixed Font and Console Issue
    
    made the ques italics
    
    changed heading style
    
    changed heading style for issues section
    
    added mentor's list
    
    Added gssoc logo
    
    added closed PR badge + changed section name
    
    added branching rules
    
    renamed important resources to useful resources
    
    added hall of fame
    
    added admin pic
    
    added admin info
    
    added link to admin's pic
    
    added missing mentor's info
    
    added new mentor's name
    
    added link to resources for node.js
    
    :up: change css inline and external to bootstrap

---
+++ react-frontend/src/components/Sticker.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const Sticker = styled.div`
+  background-color: ${props => props.backgroundColor};
+`
---
+++ react-frontend/src/components/Tank.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const TankCircle = styled.div`
+  height: 200px;
+  width: 200px;
+  border-color: ${props => props.borderColor || "red"};
+  border-width: 2px;
+  border-style: solid;
+`
+
+export const TankWaterValue = styled.p``
