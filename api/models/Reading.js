bad745eecb90db43147a247aaf531e3865c86be2
Arpit Jain <arpitkotecha1999@gmail.com>
2020-03-04 01:14:33 +0530

    added use strict in api js files

--- api/models/Reading.js
+++ api/models/Reading.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const mongoose = require("mongoose");
 
 const readingSchema = new mongoose.Schema({
--- api/server.js
+++ api/server.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const express = require("express"),
 mongoose = require("mongoose"),
 bodyParser = require("body-parser"),
