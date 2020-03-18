373a9fb8599cd3c3a1969ee38e6f461e1e6f3366
lakshyajit <lakshyajit165@gmail.com>
2020-03-18 21:00:31 +0530

    Modified the reading model
    updating changes to reading model
    
    fix
    
    updated reading model

--- api/models/Reading.js
+++ api/models/Reading.js
@@ -6,6 +6,22 @@ const readingSchema = new mongoose.Schema({
   value: {
     required: true,
     type: String
+  },
+  tankId: {
+    required: true,
+    type: mongoose.Types.ObjectId
+  },
+  tankCapacity: {
+    required: true,
+    type: Number
+  },
+  tankLocation: {
+    required: true,
+    type: String
+  },
+  tankName: {
+    required: true,
+    type: String
   }
 });
 
