caccae75e22839923448133950df2ba73d8aa376
root <biswas.manali8@gmail.com>
2020-03-22 15:20:12 +0000

    linted code

--- api/routes/tankroutes.js
+++ api/routes/tankroutes.js
@@ -1,32 +1,40 @@
 "use strict";
-const express=require("express");
-const router =express.Router();
-const Tank=require("../models/tank");
+const express = require("express");
+const router = express.Router();
+const Tank = require("../models/tank");
 
-/* Adds a new tank according to specifications.
+/**
+ * Adds a new tank according to specifications.
  * @param {Number} capacity-tank capacity
  * @param {String} name-tank name
  * @param {String} location-tank location
- * @returns {Tank} Tank object
- * @throws {Internal Error} When tank is not created.*/ 
-router.post("/add",(req,res)=>{  
+ * @returns {Object} Tank object 
+ * @throws {NotFoundError} When tank is not created.
+ */
+router.post("/add", function (req, res) {
 	var name = req.body.name,
-		location=req.body.location,
-		capacity=req.body.capacity;
-	
+		location = req.body.location,
+		capacity = req.body.capacity;
+
 	Tank.create({
 		capacity: capacity,
-		name:name,
-		location:location
-	}, function(err,tank){
-		if(err){ res.json({status:"500",
-						   message:"Internal Server Error!"});}
+		name: name,
+		location: location
+	}, function (err, tank) {
+		if (err) {
+			res.json({
+				status: "500",
+				message: "Internal Server Error!"
+			});
+		}
 		else {
-			res.json({status:"200",
-					  message:"",
-					  data:tank});
+			res.json({
+				status: "200",
+				message: "",
+				data: tank
+			});
 		}
 	});
 });
 
-module.exports=router;
+module.exports = router;
