51711add1083433480d553ff60bbf98db6f92e01
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-04-15 00:47:40 +0530

    hardware model apis

--- water_monitoring_system/water_monitoring_system/hardware/api/serializers.py
+++ water_monitoring_system/water_monitoring_system/hardware/api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from water_monitoring_system.hardware.models import Hardware
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class HardwareSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Hardware
+        fields = ['hardware_id', 'water_level', 'latitude', 'longitude', 'user_id']
