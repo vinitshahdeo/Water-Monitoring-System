df1977a80bcec15e167f0539748c3bf564ccce18
unknown <ashishnagpal2498@gmail.com>
2020-05-13 03:10:12 +0530

    :up: correction

--- react-frontend/src/pages/Home.js
+++ react-frontend/src/pages/Home.js
@@ -13,32 +13,26 @@ class Home extends Component {
     return (
       <div>
         <div className="container">
-          <div className="row m-3">
-            <center>
+          <div className="row m-3 justify-content-center">
               <h1 className="title">Water Monitoring System</h1>
-              <p>
+              <p className="text-center">
                 Water Monitoring System is an IOT based Liquid Level Monitoring
                 system that has mechanisms to keep the user alerted in case of
                 liquid overflow or when tank depletes.
               </p>
-            </center>
           </div>
-          <div className="row m-5 d-flex justify-content-center align-items-center">
+          <div className="row mt-5 mb-5 pr-3 pl-3 d-flex justify-content-center align-items-center">
             {this.props.tanks.map((tank, index) => (
-              <div key={index}>
-                <div
-                  className="col-lg-3 d-flex align-items-center"
+            <div key={index}
+                  className="col d-flex row justify-content-center align-items-center"
                   id={tank.id}
                 >
-                  <center>
                     <TankCircle className="rounded-circle">
                       <TankWaterValue className="grey-color d-flex justify-content-center align-items-center w-100 h-100 position-relative">{tank.value}%</TankWaterValue>
                     </TankCircle>
-                    <div className="d-flex justify-content-center">
-                      <span className="tank-title">Tank{tank.id}</span>
+                    <div className="col-12 mt-2 mb-4 mb-lg-0 text-center tank-title p-0">
+                     Tank{tank.id}
                     </div>
-                  </center>
-                </div>
               </div>
             ))}
           </div>
@@ -56,8 +50,8 @@ class Home extends Component {
             </div>
           </div>
           <hr/>
-          <p>
-            <center><strong>Made by Vinit Shahdeo</strong></center>
+          <p className="text-center">
+            <strong>Made by Vinit Shahdeo</strong>
           </p>
         </div>
       </div>
