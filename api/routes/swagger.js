e4761736273f8ff1b79962eac0da0da7672b3e2b
anandhakrishnanaji <creattech2000@gmail.com>
2020-08-21 09:19:16 +0530

    Included contact details

--- api/routes/swagger.js
+++ api/routes/swagger.js
@@ -12,15 +12,14 @@ const swaggerOptions = {
         title: "Water Monitoring System",
         version: "1.0.0",
         description:
-          "A test project to understand how easy it is to document and Express API",
+          "Water Monitoring System is an IOT based Liquid Level Monitoring system that has mechanisms to keep the user alerted in case of liquid overflow or when tank depletes. The water tanks can be fixed with ultrasonic sensors that is placed over the container. Ultrasonic sensor is used to measure, compare container depth and liquid level.",
         license: {
           name: "MIT",
           url: "https://choosealicense.com/licenses/mit/"
         },
         contact: {
-          name: "Swagger",
-          url: "https://swagger.io",
-          email: "Info@SmartBear.com"
+          name: "Vinit Shahdeo",
+          url: "https://github.com/vinitshahdeo",
         }
       },
       servers: [
